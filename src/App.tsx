9efe8a79941e4867df8e71f7cac07f2200470a2e
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-25 22:36:07 -0500

    Install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+        <Route path="/login" />
+        <Route path="/signup" />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
