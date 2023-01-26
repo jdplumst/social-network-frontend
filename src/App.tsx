85bea920e2266b006925045b9547e6b538ffd797
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-26 00:27:29 -0500

    Add Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Intro from "./pages/Intro";
+import Login from "./pages/Login";
 import "./styles/index.css";
 
 const App = () => {
@@ -8,7 +9,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Intro />} /> // Intro, redirect to Home if
         loggedin
-        <Route path="/login" /> // Login, redirect to Home if loggedin
+        <Route path="/login" element={<Login />} /> // Login, redirect to Home
+        if loggedin
         <Route path="/signup" /> // Signup, redirect to Home if loggedin
         <Route path="/home" /> // Home, redirect to Intro if loggedin
       </Routes>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,24 @@
+const Login = () => {
+  return (
+    <div className="bg-slate-300 min-h-screen flex justify-center items-center pt-10">
+      <form className="flex flex-col p-10 bg-white w-1/2 h-1/3">
+        <h3 className="text-2xl text-center pb-5 font-bold">Log In</h3>
+        <label className="font-bold">Email:</label>
+        <input
+          type="email"
+          className="p-2 mb-5 border-solid border-2 border-slate-200 focus:border-slate-500 focus:outline-none rounded-lg block w-full"
+        />
+        <label className="font-bold">Password:</label>
+        <input
+          type="password"
+          className="p-2 mb-5 border-solid border-2 border-slate-200 focus:border-slate-500 focus:outline-none rounded-lg block w-full"
+        />
+        <button className="bg-blue-500 hover:bg-blue-700 hover:cursor-pointer text-white p-4 rounded-lg font-bold w-1/4 mx-auto">
+          Log In
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
