920924437417b2b5fbf09381b9dfe14e90e4199d
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-27 01:04:51 -0500

    Handle signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { UserContext } from "./contexts/UserContext";
 import Intro from "./pages/Intro";
 import Login from "./pages/Login";
 import OnboardingInfo from "./pages/OnboardingInfo";
@@ -6,6 +8,8 @@ import Signup from "./pages/Signup";
 import "./styles/index.css";
 
 const App = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -13,8 +17,10 @@ const App = () => {
         loggedin
         <Route path="/login" element={<Login />} /> // Login, redirect to Home
         if loggedin
-        <Route path="/signup" element={<Signup />} /> // Signup, redirect to
-        Home if loggedin
+        <Route
+          path="/signup"
+          element={!user.token ? <Signup /> : <OnboardingInfo />}
+        />
         <Route path="/onboarding/info" element={<OnboardingInfo />} /> //
         OnboardingInfo, redirect to Signup if not signed up yet
         <Route path="/home" /> // Home, redirect to Intro if loggedin
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,21 +1,48 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { user, setUser } = useContext(UserContext);
+
+  const handleSignup = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/user/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: email, password: password })
+    });
+
+    const data = await response.json();
+    // console.log(data);
+
+    if (!response.ok) {
+      console.log("ERROR!");
+    } else {
+      setUser(data);
+    }
+  };
 
   return (
     <div className="bg-slate-300 min-h-screen flex justify-center items-center pt-10">
-      <form className="flex flex-col p-10 bg-white w-1/2 h-1/3">
+      <form
+        onSubmit={handleSignup}
+        className="flex flex-col p-10 bg-white w-1/2 h-1/3">
         <h3 className="text-2xl text-center pb-5 font-bold">Sign Up</h3>
         <label className="font-bold">Email:</label>
         <input
           type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
           className="p-2 mb-5 border-solid border-2 border-slate-200 focus:border-slate-500 focus:outline-none rounded-lg block w-full"
         />
         <label className="font-bold">Password:</label>
         <input
           type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
           className="p-2 mb-5 border-solid border-2 border-slate-200 focus:border-slate-500 focus:outline-none rounded-lg block w-full"
         />
         <button className="bg-green-500 hover:bg-green-700 hover:cursor-pointer text-white p-4 rounded-lg font-bold w-1/4 mx-auto">
