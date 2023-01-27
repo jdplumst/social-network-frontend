4ce86678653d427d6adae7ebfc3d1d3953e4ea16
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-27 11:17:06 -0500

    Add Home page and route navigation for existing pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { UserContext } from "./contexts/UserContext";
+import Home from "./pages/Home";
 import Intro from "./pages/Intro";
 import Login from "./pages/Login";
 import OnboardingInfo from "./pages/OnboardingInfo";
@@ -18,16 +19,22 @@ const App = () => {
         loggedin
         <Route
           path="/login"
-          element={!user.token ? <Login /> : <OnboardingInfo />}
-        />{" "}
-        // Login, redirect to Home if loggedin
+          element={!user.token ? <Login /> : <Navigate to="/home" />}
+        />
         <Route
           path="/signup"
-          element={!user.token ? <Signup /> : <OnboardingInfo />}
+          element={
+            !user.token ? <Signup /> : <Navigate to="/onboarding/info" />
+          }
         />
-        <Route path="/onboarding/info" element={<OnboardingInfo />} /> //
-        OnboardingInfo, redirect to Signup if not signed up yet
-        <Route path="/home" /> // Home, redirect to Intro if loggedin
+        <Route
+          path="/onboarding/info"
+          element={user.token ? <OnboardingInfo /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/home"
+          element={user.token ? <Home /> : <Navigate to="/" />}
+        />
       </Routes>
     </BrowserRouter>
   );
