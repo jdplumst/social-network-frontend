6f9be6c879fc536a22505dbcbd09ada71aef93da
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-03-03 17:01:02 -0500

    Add Navbar and Profile Info to Profile page and make sure serach bar autocomplete works

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,12 +2,16 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 import { ProfileContext, IProfile } from "../contexts/ProfileContext";
+import Navbar from "../components/Navbar";
+import useGetProfiles from "../hooks/useGetProfiles";
 
 const Profile = () => {
   const { id } = useParams();
   const { user } = useContext(UserContext);
   const { profile } = useContext(ProfileContext);
+  const { getProfiles } = useGetProfiles();
   const [currProfile, setCurrProfile] = useState({} as IProfile);
+  const [following, setFollowing] = useState(false);
   const [error, setError] = useState<null | string>(null);
 
   useEffect(() => {
@@ -24,32 +28,71 @@ const Profile = () => {
       } else {
         setError(null);
         setCurrProfile(data);
+        console.log("fetched profile!");
       }
     };
     if (user.token) {
       getProfile();
+      getProfiles();
     }
-  }, [id, user]);
+  }, [id, user, getProfiles]);
 
   return (
-    <>
+    <div>
+      <Navbar />
       {error ? (
         <div>{error}</div>
       ) : (
-        <div>
-          <h1>This is the profile page!</h1>
-          <p>
-            The id is {id} {typeof id}
-          </p>
-          {parseInt(profile.user_id) === parseInt(id || "-1") && (
-            <p>This is your own profile!</p>
-          )}
-          <p>{currProfile.user_id}</p>
-          <p>{currProfile.first_name}</p>
-          <p>{currProfile.last_name}</p>
+        <div className="flex flex-col items-center bg-slate-300 min-h-screen">
+          <div className="p-5">
+            <img
+              src={currProfile.profile_picture}
+              alt="pic"
+              className="w-52 h-52 inline mx-2"
+            />
+          </div>
+          <div className="bg-white min-w-1/2 border-2 border-black">
+            <h3 className="text-3xl font-bold text-center">
+              {currProfile.first_name} {currProfile.last_name}
+            </h3>
+            <div className="grid grid-cols-3 p-5 gap-5">
+              <p>
+                <strong>Location: </strong>
+                {currProfile.location}
+              </p>
+              <p>
+                <strong>Occupation: </strong>
+                {currProfile.occupation}
+              </p>
+              <p>
+                <strong>Followers: </strong>0
+              </p>
+              <p>
+                <strong>Gender: </strong>
+                {currProfile.gender}
+              </p>
+              <p>
+                <strong>Birthday: </strong>{" "}
+                {new Date(currProfile.birthday).toLocaleDateString()}
+              </p>
+              <p>
+                <strong>Following: </strong>0
+              </p>
+            </div>
+            <div className="flex justify-center pb-5">
+              <button
+                className={`hover:cursor-pointer ${
+                  following
+                    ? `bg-gradient-to-tr from-cyan-500 via-blue-500 to-purple-500 hover:bg-gradient-to-tr hover:from-cyan-700 hover:via-blue-700 hover:to-purple-700`
+                    : `bg-purple-500 hover:bg-purple-700`
+                } text-white px-2 py-1 rounded-lg font-bold w-1/5`}>
+                {following ? `Following` : `Follow`}
+              </button>
+            </div>
+          </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
